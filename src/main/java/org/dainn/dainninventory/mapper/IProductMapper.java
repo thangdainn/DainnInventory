73e2cf321fe023e82ce4ae94540a3641a70454ee
ThangDulcie <112870043+ThangDulcie@users.noreply.github.com>
2024-12-16 22:43:43 +0700

    fix: remove named

--- src/main/java/org/dainn/dainninventory/mapper/IProductMapper.java
+++ src/main/java/org/dainn/dainninventory/mapper/IProductMapper.java
@@ -2,19 +2,16 @@ package org.dainn.dainninventory.mapper;
 
 import org.dainn.dainninventory.controller.request.ProductRequest;
 import org.dainn.dainninventory.dto.ProductDTO;
-import org.dainn.dainninventory.entity.BrandEntity;
-import org.dainn.dainninventory.entity.CategoryEntity;
 import org.dainn.dainninventory.entity.ProductEntity;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
-import org.mapstruct.Named;
 
 @Mapper(componentModel = "spring")
 public interface IProductMapper {
     ProductEntity toEntity(ProductDTO request);
-    @Mapping(target = "brandId", source = "brand", qualifiedByName = "toBrandDTO")
-    @Mapping(target = "categoryId", source = "category", qualifiedByName = "toCategoryDTO")
+    @Mapping(target = "brandId", source = "brand.id")
+    @Mapping(target = "categoryId", source = "category.id")
     ProductDTO toDTO(ProductEntity entity);
     ProductDTO toDTO(ProductRequest request);
 
@@ -22,19 +19,4 @@ public interface IProductMapper {
     @Mapping(target = "createdDate", ignore = true)
     @Mapping(target = "modifiedDate", ignore = true)
     ProductEntity updateEntity(@MappingTarget ProductEntity entity, ProductDTO request);
-
-    @Named("toCategoryDTO")
-    default Integer toCategoryDTO(CategoryEntity category) {
-        if (category != null) {
-            return category.getId();
-        }
-        return null;
-    }
-    @Named("toBrandDTO")
-    default Integer toBrandDTO(BrandEntity brand) {
-        if (brand != null) {
-            return brand.getId();
-        }
-        return null;
-    }
 }
